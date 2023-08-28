f07ef833793fb1efe68331ef5ad837edd5f80129
platonovvi <72863218+platonovvi@users.noreply.github.com>
2023-08-28 12:51:39 +0500

    fix

--- server.js
+++ server.js
@@ -1,23 +1,17 @@
 const express = require('express');
 const path = require('path');
+const cors = require('cors');
 const app = express();
 
-// Middleware для настройки CORS
-app.use((req, res, next) => {
-    res.header('Access-Control-Allow-Origin', 'https://tranquil-island-01847-9479261fef91.herokuapp.com');
-    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
-    if (req.method === 'OPTIONS') {
-        res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
-        return res.status(200).json({});
-    }
-    next();
-});
+// Используем cors middleware для настройки CORS
+app.use(cors());
 
 // Serve static files from the 'dist' directory
 app.use(express.static(path.join(__dirname, 'dist')));
 
 // Redirect all requests to the 'index.html'
 app.get('*', (req, res) => {
+    console.log('Received a request:', req.url);
     res.sendFile(path.join(__dirname, 'dist/index.html'));
 });
 
