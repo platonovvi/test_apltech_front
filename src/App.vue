ad13c78edefe8a03bfce220575616f5013e7da73
platonovvi <72863218+platonovvi@users.noreply.github.com>
2023-08-31 01:24:49 +0500

    asdq

--- src/App.vue
+++ src/App.vue
@@ -15,7 +15,7 @@ export default {
     async getUsers() {
       axios({
         method: 'get',
-        url: 'https://sleepy-dawn-85022-dfcee393bc59.herokuapp.com/',
+        url: 'https://sleepy-dawn-85022-dfcee393bc59.herokuapp.com/user/get_all',
       }).then(response => {
         console.log(response);
       }).catch(error => {
