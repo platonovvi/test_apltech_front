407b7d0bb82ced8d8c9aff49e94bff65704c902e
platonovvi <72863218+platonovvi@users.noreply.github.com>
2023-09-06 23:05:22 +0500

    test_axios

--- src/App.vue
+++ src/App.vue
@@ -15,7 +15,7 @@ export default {
     async getUsers() {
       axios({
         method: 'get',
-        url: 'https://sleepy-dawn-85022-dfcee393bc59.herokuapp.com/user/get_all',
+        url: 'https://sleepy-dawn-85022-dfcee393bc59.herokuapp.com/asd',
       }).then(response => {
         console.log(response);
       }).catch(error => {
