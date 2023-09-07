06cfe9d47f995fcaf27e7d450772337d17006408
platonovvi <72863218+platonovvi@users.noreply.github.com>
2023-09-08 01:27:30 +0500

    change server.js

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const path = require('path');
 const cors = require('cors');
-const fetch = require('node-fetch');
+const fetch = require("node-fetch");
 const app = express();
 
 // Используем cors middleware для настройки CORS для конкретного домена
@@ -12,40 +12,44 @@ app.use(cors({
     credentials: true,
     maxAge: 86400,
 }));
-
-// Парсим JSON входящего запроса
-app.use(express.json());
-
-// Путь для обработки GET-запросов
-app.get('*', (req, res) => {
+// Serve static files from the 'dist' directory
+app.use(express.static(path.join(__dirname, 'dist')));
+// Redirect all requests to the 'index.html'
+/*app.get('*', (req, res) => {
     res.sendFile(path.join(__dirname, 'dist/index.html'));
-});
-
-// Путь для обработки POST-запросов
-app.post('*', (req, res) => {
-    const postData = req.body;
-
-    // Просто перенаправляем данные на тот же URL, на который пришел запрос
-    fetch(req.url, {
-        method: 'POST',
-        headers: {
-            'Content-Type': 'application/json',
-        },
-        body: JSON.stringify(postData),
-    })
-        .then(response => {
-            // Обработка ответа от сервера, если необходимо
-            return response.json();
+});*/
+app.all('*', (req, res) => {
+    // Получите тип запроса (GET, POST, PUT, PATCH, DELETE и т. д.)
+    const requestMethod = req.method;
+    // В зависимости от типа запроса, выполните разные действия
+    if (requestMethod === 'GET') {
+        res.sendFile(path.join(__dirname, 'dist/index.html'));
+    } else if (requestMethod === 'POST') {
+        const postData = req.body;
+        // Просто перенаправляем данные на тот же URL, на который пришел запрос
+        fetch(req.url, {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify(postData),
         })
-        .then(data => {
-            // Отправляем ответ обратно клиенту, если необходимо
-            res.json(data);
-        })
-        .catch(error => {
-            // Обработка ошибки, если отправка данных не удалась
-            console.error('Error:', error);
-            res.status(500).json({ error: 'Failed to send data' });
-        });
+            .then(response => {
+                // Обработка ответа от сервера, если необходимо
+                return response.json();
+            })
+            .then(data => {
+                // Отправляем ответ обратно клиенту, если необходимо
+                res.json(data);
+            })
+            .catch(error => {
+                // Обработка ошибки, если отправка данных не удалась
+                console.error('Error:', error);
+                res.status(500).json({error: 'Failed to send data'});
+            });
+    } else if (requestMethod === 'PATCH') {
+    } else {
+    }
 });
 
 const port = process.env.PORT || 3000;
