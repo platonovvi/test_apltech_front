78a8e4bfe7047de48c3f77d4b411580611de279d
platonovvi <72863218+platonovvi@users.noreply.github.com>
2023-09-08 01:39:23 +0500

    change server.js

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const path = require('path');
 const cors = require('cors');
-const fetch = require("node-fetch");
+fetch = require("node-fetch");
 const app = express();
 
 // Используем cors middleware для настройки CORS для конкретного домена
