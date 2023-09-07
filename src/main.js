0a9ce4805de029cd64cf08f8b90ca6f82aa1bbbf
platonovvi <72863218+platonovvi@users.noreply.github.com>
2023-09-07 12:35:26 +0500

    add DevTools

--- src/main.js
+++ src/main.js
@@ -5,4 +5,9 @@ import axios from './axios'; // Импортируем Axios
 const app = createApp(App);
 app.config.globalProperties.$axios = axios;
 app.use(router);
+
+import VueDevtools from 'vuejs-devtools'
+if (process.env.NODE_ENV === 'development') {
+    VueDevtools.connect('', app)
+}
 app.mount('#app');
