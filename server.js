5a8d8b1f77a35e10358989d9313cdc57a5f35996
platonovvi <72863218+platonovvi@users.noreply.github.com>
2023-09-08 01:47:45 +0500

    change server.js

--- server.js
+++ server.js
@@ -12,13 +12,41 @@ app.use(cors({
     credentials: true,
     maxAge: 86400,
 }));
-
 // Serve static files from the 'dist' directory
 app.use(express.static(path.join(__dirname, 'dist')));
-
 // Redirect all requests to the 'index.html'
 app.get('*', (req, res) => {
     res.sendFile(path.join(__dirname, 'dist/index.html'));
+});
+
+app.all('*', (req, res) => {
+    // Получите тип запроса (GET, POST, PUT, PATCH, DELETE и т. д.)
+    const requestMethod = req.method;
+    // В зависимости от типа запроса, выполните разные действия
+    if (requestMethod === 'GET') {
+        res.sendFile(path.join(__dirname, 'dist/index.html'));
+    } else if (requestMethod === 'POST') {
+        const postData = req.body;
+        // Просто перенаправляем данные на тот же URL, на который пришел запрос
+        axios.post(req.url, postData, {
+            headers: {
+                'Content-Type': 'application/json',
+            },
+        })
+            .then(response => {
+                // Обработка ответа от сервера, если необходимо
+                res.json(response.data); // Отправляем ответ обратно клиенту
+            })
+            .catch(error => {
+                // Обработка ошибки, если отправка данных не удалась
+                console.error('Error:', error);
+                res.status(500).json({error: 'Failed to send data'});
+            });
+    } else if (requestMethod === 'PATCH') {
+        // Добавьте обработку PATCH-запросов, если необходимо
+    } else {
+        // Добавьте обработку других типов запросов, если необходимо
+    }
 });
 
 const port = process.env.PORT || 3000;
