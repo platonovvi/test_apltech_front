5926bb3082c2fd02093f35aa1c7d937631908d0c
platonovvi <72863218+platonovvi@users.noreply.github.com>
2023-09-07 13:45:04 +0500

    server request

--- src/main.js
+++ src/main.js
@@ -2,10 +2,10 @@ import { createApp } from 'vue'
 import App from './vue/root_app.vue'
 import router from './router';
 import axios from './axios'; // Импортируем Axios
-import Swal from 'sweetalert2'; //Импортируем SweetAlert
+import VueSweetalert2 from 'vue-sweetalert2'; //Импортируем SweetAlert
 import 'sweetalert2/dist/sweetalert2.min.css';
 const app = createApp(App);
 app.config.globalProperties.$axios = axios;
-app.config.globalProperties.$swal  = Swal;
+app.use(VueSweetalert2);
 app.use(router);
 app.mount('#app');
