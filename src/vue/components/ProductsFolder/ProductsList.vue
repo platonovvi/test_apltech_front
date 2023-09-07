0b595bd1c684e95f9e5616be10528e9f6f50e402
platonovvi <72863218+platonovvi@users.noreply.github.com>
2023-09-07 13:37:38 +0500

    server request

--- src/vue/components/ProductsFolder/ProductsList.vue
+++ src/vue/components/ProductsFolder/ProductsList.vue
@@ -40,7 +40,7 @@ export default {
   methods: {
     async getProducts() {
       try {
-        const response = await this.$root.request({
+        let response = await this.$root.request({
           url: '/products',
           method: 'GET',
         });
@@ -56,7 +56,7 @@ export default {
     async getProduct() {
       if (this.data.search_id) {
         try {
-          const response = await new this.$root.request({
+          let response = await this.$root.request({
             url: '/product/' + this.data.search_id,
             method: 'GET',
           });
