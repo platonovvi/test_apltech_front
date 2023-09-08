0ba74588164542ab9af4225cb6a670658786aabe
platonovvi <72863218+platonovvi@users.noreply.github.com>
2023-09-08 11:12:52 +0500

    add JWT

--- server.js
+++ server.js
@@ -5,19 +5,6 @@ const axios = require('axios');
 const jwt = require('jsonwebtoken');
 const app = express();
 
-// Настройки секретного ключа для подписи и проверки JWT
-const secretKey = 'your-secret-key';
-
-// Роут для создания JWT после успешной аутентификации
-app.post('/login', (req, res) => {
-    // Ваша логика аутентификации здесь
-
-    // Генерация JWT
-    const token = jwt.sign({userId: user.id}, secretKey, {expiresIn: '1h'});
-
-    res.json({token});
-});
-
 // Используем cors middleware для настройки CORS для конкретного домена
 app.use(cors({
     origin: 'https://tranquil-island-01847-9479261fef91.herokuapp.com',
@@ -31,16 +18,6 @@ app.use(express.static(path.join(__dirname, 'dist')));
 // Redirect all requests to the 'index.html'
 app.get('*', (req, res) => {
     res.sendFile(path.join(__dirname, 'dist/index.html'));
-});
-app.post('/user/auth', async (req, res) => {
-    try {
-        const dataFromVue = req.body;
-        const response = await axios.post('https://sleepy-dawn-85022-dfcee393bc59.herokuapp.com/user/auth', dataFromVue);
-        res.json(response.data);
-    } catch (error) {
-        console.error('Error:', error);
-        res.status(500).json({message: 'Internal Server Error'});
-    }
 });
 app.post('/user/signup', async (req, res) => {
     try {
